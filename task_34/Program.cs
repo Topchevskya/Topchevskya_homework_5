c7de67540988987af95bd5cffdbc2d9867179eca
Elena <Dafinia@mail.ru>
2022-08-23 23:31:35 +0300

    HW

---
+++ task_34/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[10];
+Random rnumber = new Random();
+int result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnumber.Next(100, 1000);
+    Console.Write($"{array[i]} ");
+    {
+        if (array[i] % 2 == 0) result++;
+    }
+}
+Console.WriteLine($" -> количество чётных чисел {result}");
---
+++ task_36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+Random rnumber = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnumber.Next(0,50);
+    Console.Write($"{array[i]} ");
+}
+
+int result = 0;
+for (int i = 0; i < array.Length; i += 2)
+{
+    result = result + array[i];
+}
+
+Console.WriteLine($"-> {result}");
